23a6626499ec45e4ad27fd6184994e1e441bc0fb
ali3amer <42501649+ali3amer@users.noreply.github.com>
2022-02-07 21:12:11 +0200

    07-02-2022 night

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -22,7 +22,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard/index';
+    public const HOME = '/';
 
     /**
      * Define your route model bindings, pattern filters, etc.
